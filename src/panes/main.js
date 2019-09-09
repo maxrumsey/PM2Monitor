7f19bc67a0dee0905fcafbd509313ed32d926e6c
Max Rumsey <max@maxrumsey.xyz>
2019-09-09 08:11:23 +0800

    Adding start/stop buttons

--- src/panes/main.js
+++ src/panes/main.js
@@ -14,10 +14,11 @@ module.exports = async (opts, manager) => {
   console.log(config)
   await ssh.connect(config)
   const input = await ssh.execCommand('cd PM2Monitor/server/; node server-connector.js -t list');
+  console.log(input);
   const apps = JSON.parse(input.stdout)
   console.log(apps)
   removeLoadingText(manager)
-  createProcList(manager.paneElement, apps)
+  createProcList(manager.paneElement, apps, ssh)
 }
 function loadKeyFile(val) {
   val = val.replace('~', remote.app.getPath('home'));
@@ -27,26 +28,33 @@ function removeLoadingText(manager) {
   const loadingText = document.getElementById('loadingText');
   manager.paneElement.removeChild(loadingText);
 }
-function createProcList(element, apps) {
+function createProcList(element, apps, ssh) {
   for (var i = 0; i < apps.length; i++) {
     const app = apps[i];
     const divContainer = document.createElement('div');
     divContainer.classList.add('app');
     const h3Title = document.createElement('h3');
-    const cycleButton = document.createElement('button');
-    const cycleButtonText = document.createTextNode('Cycle Status');
-
-    cycleButton.addEventListener('click', function() {
-      console.log('Cycling Proc ID: ' + app.pm_id)
-    })
-    
     const h3TitleText = document.createTextNode(app.pm_id + ': ' + app.name);
     const statusText = document.createTextNode(app.pm2_env.status);
     h3Title.appendChild(h3TitleText);
     divContainer.appendChild(h3Title);
     divContainer.appendChild(statusText);
-    cycleButton.appendChild(cycleButtonText);
-    divContainer.appendChild(cycleButton);
+
+    createButton(divContainer, 'Stop', async () => {
+      console.log(await ssh.execCommand('cd PM2Monitor/server/; node server-connector.js -t stop --id ' + app.pm_id));
+    })
+    createButton(divContainer, 'Restart / Start', async () => {
+      console.log(await ssh.execCommand('cd PM2Monitor/server/; node server-connector.js -t restart --id ' + app.pm_id));
+    })
+    
     element.appendChild(divContainer);
   }
+}
+function createButton(top, text, fn) {
+  const button = document.createElement('button');
+  const buttonText = document.createTextNode(text);
+
+  button.addEventListener('click', fn);
+  button.appendChild(buttonText)
+  top.appendChild(button)
 }
